249ef25c815cf2f92e05fa4fc219453856cf6a21
pranay101 <pranayprajapati101@gmail.com>
2025-06-15 23:18:19 +0530

    Initial Commit, Project Initialization and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+   <main>
+    <section>
+      <h4 className='bg-red-400'>pomoodoro activity</h4>
+    </section>
+   </main>
+  )
+}
+
+export default App
